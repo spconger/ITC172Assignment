9bc332f4c761496d3c4f3fff0891c78e19327697
Steve Conger <steve@spconger.com>
2017-04-06 12:17:28 -0700

    Commit tip

---
+++ App_Code/Tip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Tip
+/// </summary>
+public class Tip
+{
+    private double mealAmount;
+    private double tipPercent;
+    private const double TAXPERCENT = .101;
+    public Tip(double amount, double tipPerc)
+    {
+        mealAmount = amount;
+        tipPercent = tipPerc;
+    }
+
+    public double CalculateTax()
+    {
+        return mealAmount * TAXPERCENT;
+    }
+
+    public double CalculateTip()
+    {
+        return mealAmount * tipPercent;
+    }
+
+    public double Total()
+    {
+        return mealAmount + CalculateTax() + CalculateTip();
+    }
+}
